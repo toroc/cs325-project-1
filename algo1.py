d6c20838c62e3bced2f612abdf6032a70259f474
David <davidmoongit@gmail.com>
2016-04-12 19:19:22 -0700

    Added files via upload
    Split algo 1&2 into two separate files.  Ran the tests, and updated the test file locally.  I'll update the test file on git next.

---
+++ algo1.py
@@ -0,0 +1,42 @@
+
+        
+def maxSumSubarray_1(arr=[]):
+    """
+    Description: Algorithm 1 - Enumeration
+       Computes the sum of all possible subarrays and returns the array with the
+       largest sum.  Stores the largest sum found so far in the maxSum variable.
+    Input: arr - an array of ints
+    Output: Returns the start index of the subarray, end index of the subarray, and the sum of the subarray
+    Complexity: O(n^3)
+    """
+
+    #if the array is empty, return 0
+    if len(arr) == 0:
+        return []
+    
+    #if the array only has one element, return that element
+    elif len(arr) == 1:
+        return arr[0]
+
+    maxSum = arr[0]  #the largest sum of a subarray of arr
+    start = 0        #the beginning index for a subarray of arr
+    stop = 0         #the end index for a subarray arr
+
+    #compute the sum for all possible pairs of indices (i,j) such that 0<=i<j
+    for i in range(len(arr) - 1):
+        for j in range(i + 1, len(arr)):
+            curSum = sum(arr[i:j+1])
+            if curSum > maxSum:
+                start = i
+                stop = j
+                maxSum = curSum
+
+    #check to see if the last element in arr is larger than all other subarray sums
+    #i.e. like for the array [-2,-5,-11,5]
+    if arr[len(arr) - 1] > maxSum:
+        maxSum = arr[len(arr) - 1]
+        start = len(arr) - 1
+        stop = len(arr) - 1
+    
+    return start, stop, maxSum
+    #return arr[start:stop+1]
