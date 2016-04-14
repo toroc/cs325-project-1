8c89723e2731409970471f5481b46f3d20726aff
Carol D. Toro <toroc@oregonstate.edu>
2016-04-13 19:39:08 -0700

    Added one function to run correctness tests and print tests to MSS_Result.txt as specified by project criteria

--- algoTests.py
+++ algoTests.py
@@ -1,81 +1,36 @@
-import processData, algo1, algo2, algo3, algo4
-
-###############################TESTING OF ALGO1 #######################
-myLists = processData.insertIntoArrays('MSS_TestProblems.txt')
-
-
-for i in range(len(myLists)):
-	testArray = myLists[i]
-	testLen = len(testArray)-1
-
-	# Test algo
-	maxAns = algo1.maxSumSubarray_1(testArray)
-	leftIdx = maxAns[0]
-	rightIdx = maxAns[1]
-
-	fullArray = testArray
-	subArray = testArray[leftIdx:rightIdx+1]
-	maxSum = maxAns[2]
-
-	processData.writeResults('algo1Results.txt', fullArray, subArray, maxSum)
+import processData
+from algo1 import *
+from algo2 import *
+from algo3 import *
+from algo4 import *
 
 
-###############################TESTING OF ALGO2 #######################
-myLists = processData.insertIntoArrays('MSS_TestProblems.txt')
+
+def runTests(fileName, algo, desc):
+
+	# Add test description to file
+
+	file = open(fileName, "a")
+	file.write("\n"+desc+"\n")
 
 
-for i in range(len(myLists)):
-	testArray = myLists[i]
-	testLen = len(testArray)-1
+	myLists = processData.insertIntoArrays('MSS_Problems.txt')
 
-	# Test algo
-	maxAns = algo2.maxSumSubarray_2(testArray)
-	leftIdx = maxAns[0]
-	rightIdx = maxAns[1]
-
-	fullArray = testArray
-	subArray = testArray[leftIdx:rightIdx+1]
-	maxSum = maxAns[2]
-
-	processData.writeResults('algo2Results.txt', fullArray, subArray, maxSum)
+	for i in range(len(myLists)):
+		testArray = myLists[i]
+		maxAns = algo(testArray)
+		subArray = maxAns[0]
+		maxSum = maxAns[1]
+		processData.writeResults(file, testArray, subArray, maxSum)
 
 
 
 
-###############################TESTING OF ALGO3 #######################
-myLists = processData.insertIntoArrays('MSS_TestProblems.txt')
 
+# Run the tests
 
-for i in range(len(myLists)):
-	testArray = myLists[i]
-	testLen = len(testArray)-1
+runTests('MSS_Results.txt', algo1, "Algorithm 1 Correctness Test Results: ")
+runTests('MSS_Results.txt', algo2, "Algorithm 2 Correctness Test Results: ")
+runTests('MSS_Results.txt', algo3, "Algorithm 3 Correctness Test Results: ")
+runTests('MSS_Results.txt', algo4, "Algorithm 4 Correctness Test Results: ")
 
-	# Test algo
-	maxAns = algo3.maxSumSubarray_3(testArray, 0, testLen)
-	leftIdx = maxAns[0]
-	rightIdx = maxAns[1]
-
-	fullArray = testArray
-	subArray = testArray[leftIdx:rightIdx+1]
-	maxSum = maxAns[2]
-
-	processData.writeResults('algo3Results.txt', fullArray, subArray, maxSum)
-
-
-###############################TESTING OF ALGO4 #######################
-myLists = processData.insertIntoArrays('MSS_TestProblems.txt')
-
-
-for i in range(len(myLists)):
-	testArray = myLists[i]
-	
-	# Test algo
-	maxAns = algo4.linMaxSubArray(testArray)
-	leftIdx = maxAns[0]
-	rightIdx = maxAns[1]
-
-	fullArray = testArray
-	subArray = testArray[leftIdx:rightIdx+1]
-	maxSum = maxAns[2]
-
-	processData.writeResults('algo4Results.txt', fullArray, subArray, maxSum)
