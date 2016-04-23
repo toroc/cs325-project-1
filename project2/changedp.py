710ec3cc6e380835cfdba1620bec7ec2b0503dda
Carol D. Toro <toroc@oregonstate.edu>
2016-04-22 19:47:22 -0700

    Change dp tested for correctness

--- project2/changedp.py
+++ project2/changedp.py
@@ -1,54 +1,50 @@
 
-INF = 1000000
-
 def changedp(coins, amount):
 
 	maxw = amount + 1
 	numCoins = len(coins)
 
-	coinUsedTable = [0 for x in range(maxw) ]
-	coinCountTable = [0 for x in range(maxw)]
+	tUsed = [0 for x in range(maxw) ]
+	tCount= [0 for x in range(maxw) ]
 	
 	# Store count of each coin value used
-	minCoinsUsed = [0 for x in range(numCoins)]
-
+	minUsed = [0 for x in range(numCoins)]
+	
 
 	for p in range(amount+1):
 
-		coinCount = p
+		count = p
 		nextCoin = 1
 
 
-		for i in [coinValue  for coinValue in coins if coinValue <= p]:
-			if (coinCountTable[ p - i] + 1 < coinCount):
-				coinCount = coinCountTable[p - i] + 1
+		for i in [val for val in coins if val <= p]:			
+			if (tCount[ p - i] + 1 < count):
+				count = tCount[p - i] + 1
 				nextCoin = i 
 
-		coinCountTable[p] = coinCount
-		coinUsedTable[p] = nextCoin
 
-	###################################Up to here the above works correctly #########################
+		tCount[p] = count
+		tUsed[p] = nextCoin
+
 
 	currVal = amount
-	# Should do this for each coin value
-
-
+	
 	while currVal > 0:
-		usedCoin = coinUsedTable[currVal]
+		usedCoin = tUsed[currVal]
 		# Parse thru the different coin values
 		j = 0
 		for coin in coins:
 			if ( coin == usedCoin):
-				minCoinsUsed[j] +=1
+					minUsed[j] +=1
 			j += 1
-
+			
 		currVal = currVal - usedCoin
 		
 		
 
-	minCoinsAnswer = coinCountTable[amount]
+	minCoinsAnswer = tCount[amount]
 
-	return minCoinsUsed, minCoinsAnswer
+	return minUsed, minCoinsAnswer
 
 
 
@@ -57,6 +53,7 @@ def changedp(coins, amount):
 coins = [1, 3, 7, 12]
 value = 29
 
-print(changedp(coins, value))
+answer = changedp(coins, value)
+print(answer)
 
 	
