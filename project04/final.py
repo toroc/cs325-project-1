7405e3fdef41b36ce262e016b7b6c227e4a14a1f
Joshua Curtis <joshua.mi.curtis@gmail.com>
2016-06-01 21:06:34 -0700

    added timer to final.py

--- project04/final.py
+++ project04/final.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # Takes as an argument the input file
-
+import time
 import sys
 from tspSimulated import readCoords1, tspSimulated, tspSimulated2
 
@@ -10,9 +10,17 @@ outFile = inFile + ".tour"
 print outFile
 cities, nodes = readCoords1(inFile)
 if (len(nodes) < 1000):
+    start = time.clock()
     cost, tour = tspSimulated(cities, nodes)
+    end = time.clock()
+    elapsed = end - start
+    print elapsed
 else:
+    start = time.clock()
     cost, tour = tspSimulated2(cities, nodes)
+    end = time.clock()
+    elapsed = end - start
+    print elapsed
 cost = str(cost)
 cost = cost + "\n"
 #tour = str(tour)
