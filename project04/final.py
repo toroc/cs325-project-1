2a0a41f58ec6d415854ddf871d03ecfa80907877
Joshua Curtis <joshua.mi.curtis@gmail.com>
2016-06-01 20:56:06 -0700

    updated final.py for different number of cities

--- project04/final.py
+++ project04/final.py
@@ -1,14 +1,18 @@
+#!/usr/bin/python
 # Takes as an argument the input file
 
 import sys
-from tspSimulated import readCoords1, tspSimulated2
+from tspSimulated import readCoords1, tspSimulated, tspSimulated2
 
 inFile = sys.argv[1]
 
 outFile = inFile + ".tour"
 print outFile
 cities, nodes = readCoords1(inFile)
-cost, tour = tspSimulated2(cities, nodes)
+if (len(nodes) < 1000):
+    cost, tour = tspSimulated(cities, nodes)
+else:
+    cost, tour = tspSimulated2(cities, nodes)
 cost = str(cost)
 cost = cost + "\n"
 #tour = str(tour)
